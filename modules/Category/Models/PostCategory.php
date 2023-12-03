660b2559d6651ca029a9b5a99bd8b7de742fedd8
Hossein Fakhari <hossein.fakhari1977@gmail.com>
2023-12-04 03:29:00 +0330

    fix in category module

---
+++ modules/Category/Models/PostCategory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Modules\Category\Models;
+
+use App\Models\Admin\Content\Post;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class PostCategory extends Model
+{
+    use HasFactory, SoftDeletes, Sluggable;
+
+    protected $guarded = [];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+
+    protected $casts = ['image' => 'array'];
+
+//    public function posts()
+//    {
+//        return $this->hasMany(Post::class);
+//    }
+}
