f4a6b493ff823bab97cb60a1ab1dfdb0b2996edf
Hossein Fakhari <hossein.fakhari1977@gmail.com>
2023-12-03 15:55:42 +0330

    fix in order module

---
+++ modules/Order/Http/Controllers/OrderController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Modules\Order\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class OrderController extends Controller
+{
+
+    public function index() {
+
+    }
+
+    public function store() {
+
+    }
+
+    public function unpaidOrders() {
+
+    }
+
+    public function sendingOrders() {
+
+    }
+
+    public function canceledOrders() {
+
+    }
+
+    public function returnedOrders() {
+
+    }
+
+    public function update() {
+
+    }
+}
