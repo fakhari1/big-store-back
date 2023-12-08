f99f6c6427b0b5f6604b8cf7b55a1b1f5a61cae9
Hossein Fakhari <hossein.fakhari1977@gmail.com>
2023-12-08 20:06:19 +0330

    fix in content module

--- modules/Content/Models/PostCategory.php
+++ modules/Content/Models/PostCategory.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PostCategory extends Model
 {
-    use HasFactory, SoftDeletes, Sluggable;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'post_categories';
 
