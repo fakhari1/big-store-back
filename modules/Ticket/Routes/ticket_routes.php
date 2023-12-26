c0a6b6d7a1a9a56562b998609f3f448ac09978a0
Hossein Fakhari <hossein.fakhari1977@gmail.com>
2023-12-26 23:36:23 +0330

    fix in ticket module

--- modules/Ticket/Routes/ticket_routes.php
+++ modules/Ticket/Routes/ticket_routes.php
@@ -10,13 +10,8 @@ Route::middleware(['api'])->prefix('api')->group(function () {
 
             Route::get('new-tickets', [TicketController::class, 'getNewTickets']);
             Route::get('open-tickets', [TicketController::class, 'getOpenTickets']);
-            Route::get('closed-tickets', [TicketController::class, 'getClosed']);
+            Route::get('closed-tickets', [TicketController::class, 'getClosedTickets']);
             Route::get('{ticket}', [TicketController::class, 'getTicket']);
-            Route::get('online', [TicketController::class, 'getOnlinePayments']);
-            Route::get('offline', [TicketController::class, 'getOfflinePayments']);
-            Route::get('on-delivered', [TicketController::class, 'getOnDeliveredPayments']);
-            Route::patch('{payment}', [TicketController::class, 'update']);
-
         });
     });
 });
