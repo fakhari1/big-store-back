222bc4410edc4e2b9a9d5c4b518091115e01d44b
Hossein Fakhari <hossein.fakhari1977@gmail.com>
2023-12-27 19:25:41 +0330

    fix in some modules

---
+++ modules/Category/Database/Migrations/2022_02_15_091837_create_product_category_default_values_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_category_default_values', function (Blueprint $table) {
+            $table->id();
+            $table->string('value');
+            $table->unsignedBigInteger('product_category_attribute_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_attribute_default_values');
+    }
+};
