be6af4b2e0c5c75945ad6f9bfeabfa9acd2850fc
Hossein Fakhari <hossein.fakhari1977@gmail.com>
2024-01-14 10:43:17 +0330

    Fix in File module

--- modules/File/Database/Migrations/2019_10_28_221149_create_files_table.php
+++ modules/File/Database/Migrations/2019_10_28_221149_create_files_table.php
@@ -4,8 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFilesTable extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -14,10 +13,10 @@ class CreateFilesTable extends Migration
     public function up()
     {
         Schema::create('files', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->id();
             $table->string('name');
-            $table->integer('size');
-            $table->integer('time')->nullable()->comment('based on seconds');
+            $table->unsignedInteger('size');
+            $table->unsignedInteger('time')->nullable()->comment('based on seconds if type is video');
             $table->string('type')->comment('video,image,archive');
             $table->boolean('is_private');
             $table->timestamps();
@@ -33,4 +32,4 @@ class CreateFilesTable extends Migration
     {
         Schema::dropIfExists('files');
     }
-}
+};
