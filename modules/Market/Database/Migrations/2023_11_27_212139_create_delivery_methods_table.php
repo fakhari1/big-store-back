e1cbce0d20b8128a24adb6cf931d5fba3d982c19
darynazar <heydari.std00@gmail.com>
2024-02-14 16:13:53 +0330

    add Delivery method

--- modules/Market/Database/Migrations/2023_11_27_212139_create_delivery_methods_table.php
+++ modules/Market/Database/Migrations/2023_11_27_212139_create_delivery_methods_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->decimal('amount', 20, 3)->nullable();
-            $table->unsignedTinyInteger('delivery_time')->nullable();
+//            $table->unsignedTinyInteger('delivery_time')->nullable();
+            $table->timestamp('delivery_time')->useCurrent();
             $table->string('delivery_time_unit')->nullable();
             $table->tinyInteger('status')->default(1);
             $table->timestamps();
