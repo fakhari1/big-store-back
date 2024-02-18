7d788256e0fffd44c7099e94654e8246ce49e97d
darynazar <heydari.std00@gmail.com>
2024-02-18 09:03:48 +0330

    add index.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,7 +34,7 @@
             <section class="d-md-flex justify-content-md-between align-items-md-center py-3">
 
                 <section class="d-flex justify-content-between align-items-center d-md-block">
-                    <a class="text-decoration-none" href="index.html"><img src="../../assets/images/logo/8.png" alt="logo"></a>
+                    <a class="text-decoration-none" href="index.blade.php"><img src="../../assets/images/logo/8.png" alt="logo"></a>
                     <button class="btn btn-link text-dark d-md-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
                         <i class="fa fa-bars me-1"></i>
                     </button>
@@ -833,7 +833,7 @@
                 <!--mobile view-->
                 <section class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel" style="z-index: 9999999;">
                     <section class="offcanvas-header">
-                        <h5 class="offcanvas-title" id="offcanvasExampleLabel"><a class="text-decoration-none" href="index.html"><img src="../../assets/images/logo/8.png" alt="logo"></a></h5>
+                        <h5 class="offcanvas-title" id="offcanvasExampleLabel"><a class="text-decoration-none" href="index.blade.php"><img src="../../assets/images/logo/8.png" alt="logo"></a></h5>
                         <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                     </section>
                     <section class="offcanvas-body">
