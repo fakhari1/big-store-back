45ec93b2f2a08e45eb1cae2a815396837ab6e002
darynazar <heydari.std00@gmail.com>
2024-02-19 10:31:59 +0330

    add vendor payment

---
+++ modules/Payment/Http/Controllers/VendorPaymentController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Modules\Payment\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use Modules\Common\Utils\Responder;
+use Modules\Market\Models\DeliveryMethod;
+use Modules\Payment\Models\OfflinePayment;
+use Modules\Payment\Models\CashPayment;
+use Modules\Payment\Models\OnlinePayment;
+use Modules\Payment\Models\Payment;
+
+class VendorPaymentController extends Controller
+{
+    public function show(Payment $payment)
+    {
+        if($payment->vendor_id == "2"){
+            return Responder::response([
+                'payment' => $payment->load(['user', 'vendor', 'paymentable']),
+            ]);
+        }
+    }
+
+    public function getOfflinePayments()
+    {
+        $payments = OfflinePayment::with(['user', 'vendor'])->get();
+
+        return Responder::response([
+            'payments' => $payments
+        ]);
+    }
+
+    public function getOnlinePayments()
+    {
+        $payments = OnlinePayment::with(['user', 'vendor'])->where('vendor_id', "2")->get();
+
+        return Responder::response([
+            'payments' => $payments
+        ]);
+    }
+
+    public function getCashPayments()
+    {
+        $payments = CashPayment::with(['user', 'vendor'])->get();
+        return Responder::response([
+            'payments' => $payments
+        ]);
+    }
+
+    public function updateStatus(OnlinePayment $payment)
+    {
+        $payment->update(['status' => 2]);
+        Payment::where('paymentable_id', $payment->id)->update(['status' => 2]);
+
+        return Responder::response([
+            'payment' => $payment
+        ]);
+    }
+}
--- modules/Payment/Routes/payment_routes.php
+++ modules/Payment/Routes/payment_routes.php
@@ -3,6 +3,8 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Payment\Http\Controllers\PaymentController;
+use Modules\Payment\Http\Controllers\VendorPaymentController;
+
 
 Route::middleware(['api'])->prefix('api')->group(function () {
 
@@ -15,6 +17,18 @@ Route::middleware(['api'])->prefix('api')->group(function () {
             Route::get('{payment}/show', [PaymentController::class, 'show']);
             Route::patch('{payment}/update-status', [PaymentController::class, 'updateStatus']);
 //                Route::patch('{payment}/update-status', [PaymentController::class, 'updateStatus']);
+        });
+    });
+
+    Route::prefix('vendor')->group(function () {
+
+        Route::prefix('payments')->group(function () {
+            Route::get('online', [VendorPaymentController::class, 'getOnlinePayments']);
+            Route::get('offline', [VendorPaymentController::class, 'getOfflinePayments']);
+            Route::get('cash', [VendorPaymentController::class, 'getCashPayments']);
+            Route::get('{payment}/show', [VendorPaymentController::class, 'show']);
+            Route::patch('{payment}/update-status', [VendorPaymentController::class, 'updateStatus']);
+//                Route::patch('{payment}/update-status', [VendorPaymentController::class, 'updateStatus']);
         });
     });
 });
